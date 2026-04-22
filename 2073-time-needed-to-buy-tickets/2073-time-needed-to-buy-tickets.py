b0724151ebbe238620e06f6fd136c1f1b1cf9f8f
Maria Jose <4mariajo@gmail.com>
2026-04-22 15:14:49 +0530

    Time: 0 ms (100%), Space: 12.4 MB (60.5%) - LeetHub

---
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def timeRequiredToBuy(self, tickets, k):
+        """
+        :type tickets: List[int]
+        :type k: int
+        :rtype: int
+        """
+        ls=tickets[k]
+        time=0
+        for i in range(len(tickets)):
+            if i>k and tickets[i]>=ls:
+                time=time+(ls-1)
+            elif tickets[i]<ls:
+                time=time+tickets[i]
+            else:
+                time+=ls
+        return time
+
