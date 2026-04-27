fb5c79f934a9de2e586da073687769402476086b
Maria Jose <4mariajo@gmail.com>
2026-04-27 12:41:09 +0530

    Time: 1 ms (30.92%), Space: 15.8 MB (43.96%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.py
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.py
@@ -14,14 +14,14 @@ class Solution(object):
 
         odd = head                 # start of odd list
         even = head.next           # start of even list
-        even_head = even           # save even head
+        even_head=head.next
 
         while even and even.next:
-            odd.next = even.next   # link next odd
-            odd = odd.next         # move odd
+            odd.next = odd.next.next  
+            even.next = even.next.next         
 
-            even.next = odd.next   # link next even
-            even = even.next       # move even
+            odd = odd.next   
+            even = even.next       
 
         odd.next = even_head       # attach even list at end
 
