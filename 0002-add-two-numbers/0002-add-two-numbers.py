d4ea996a5845b45140fc8556720a9509737cd103
Maria Jose <4mariajo@gmail.com>
2026-04-27 11:41:12 +0530

    Time: 0 ms (100%), Space: 12.4 MB (58.75%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,43 +5,18 @@
 #         self.next = next
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: Optional[ListNode]
-        :type l2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-        lis1=[]
-        lis2=[]
-
-        while l1:
-            lis1.append(l1.val)
-            l1=l1.next
-
-        #print(lis1)
-        lis1.reverse()
-        num1=int("".join(map(str, lis1)))
-        print(num1)
-
-        while l2:
-            lis2.append(l2.val)
-            l2=l2.next
-
-        #print(lis2)
-        lis2.reverse()
-        num2=int("".join(map(str, lis2)))
-        print(num2)
-
-        num3=num1+num2
-        print(num3)
-
-        lis3=list(map(int, str(num3)))
-        lis3.reverse()
-        print(lis3)
-
+        #here no need for reverse
         dummy=ListNode(0)
         temp=dummy
-
-        for item in lis3:
-            temp.next=ListNode(item)
+        carry=0
+        while l1 or l2 or carry:
+            if l1:
+                carry +=l1.val
+                l1=l1.next
+            if l2:
+                carry +=l2.val
+                l2=l2.next
+            temp.next=ListNode(carry%10)
             temp=temp.next
+            carry//=10
         return dummy.next
