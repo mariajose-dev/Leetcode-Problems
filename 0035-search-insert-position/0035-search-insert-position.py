49c3dcff2533f4ac951cac9eacb4491e7d03b308
Maria Jose <4mariajo@gmail.com>
2026-04-28 14:40:56 +0530

    Time: 0 ms (100%), Space: 13 MB (30.27%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        l = 0
+        h = len(nums) - 1
+
+        while l <= h:
+            mid = (l + h) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                h = mid - 1
+
+        return l
